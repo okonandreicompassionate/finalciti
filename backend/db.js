b6e07594d7a3a5c17d4a4e2d84e6a3f35e5f6274
Andrei Okon <okoncompassionate@gmail.com>
2026-03-14 15:14:01 +0100

    uuuu

--- backend/db.js
+++ backend/db.js
@@ -2,7 +2,7 @@ import { Pool } from "pg";
 
 const db = new Pool({
   connectionString:
-    "postgresql://neondb_owner:npg_nrEiM9hasJO3@ep-purple-breeze-a49xr1iv-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require",
+    process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false,
   },
